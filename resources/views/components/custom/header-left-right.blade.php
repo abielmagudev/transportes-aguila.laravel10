64e6d30f4c08210c8d2d97dcdec7c156f3bdea7a
Abiel Magu <abielmagu.dev@gmail.com>
2023-09-12 16:15:13 -0500

    Factoriza custom component de header-left-right

---
+++ resources/views/components/custom/header-left-right.blade.php
@@ -0,0 +1,10 @@
+<header class="d-flex justify-content-between align-items-center">
+    <div class="text-start">
+        {!! $left ?? '' !!}
+    </div>
+    <div class="text-end">
+        {!! $right ?? '' !!}
+    </div>
+</header>
+<hr>
+<br>
